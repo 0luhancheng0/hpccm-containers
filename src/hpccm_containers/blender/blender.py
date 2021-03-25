0dd43e2d0fd62823aa087b8009030b828ae96040
Luhan Cheng <lche0021@student.monash.edu>
2021-03-25 20:24:10 +1100

    update blender

--- src/hpccm_containers/blender/blender.py
+++ src/hpccm_containers/blender/blender.py
@@ -1,13 +1,11 @@
 
-import os
 from hpccm import config, Stage
-from hpccm.building_blocks import gnu, openmpi, packages, nvhpc, generic_build, python
+from hpccm.building_blocks import gnu, packages, generic_build
 from hpccm.primitives import label, baseimage, comment, runscript, shell, environment
 from fire import Fire
-from hpccm_containers.utils import add_include_path, add_library_path, from_prefix
 
 
-def build(container_format='singularity', os_release='ubuntu', os_version='18.04', blender_version='2.92.0'):
+def build(container_format='singularity', os_release='ubuntu', os_version='20.04', blender_version='2.92.0', gnu_version='10'):
     config.set_container_format(container_format)
 
     image = f'{os_release}:{os_version}'
@@ -25,18 +23,20 @@ def build(container_format='singularity', os_release='ubuntu', os_version='18.04
         '/bin/bash'
     ])
     stage0 += packages(apt=['wget', 'git', 'software-properties-common', 'build-essential', 'locales', 'zlib1g-dev'])
-    stage0 += python(python3=True, devel=True)
     stage0 += shell(commands=['locale-gen en_AU.UTF-8'])
 
     stage0 += comment('Installing vglrun and TurboVNC')
-    stage0 += packages(apt=['ubuntu-desktop', 'vim', 'mesa-utils', 'python3-pip', 'python3-pyqt5', 'pyqt5-dev', 'python3-tk'])
+    stage0 += packages(apt=['ubuntu-desktop', 'vim', 'mesa-utils', 'python3-pip', 'python3-pyqt5', 'pyqt5-dev', 'python3-tk', 'python3.8-dev'])
+    stage0 += environment(variables={
+        "CPATH": "/usr/include/python3.8:$CPATH"
+    })
     stage0 += shell(commands=[
         'wget https://swift.rc.nectar.org.au/v1/AUTH_810/CVL-Singularity-External-Files/turbovnc_2.2.5_amd64.deb && dpkg -i turbovnc_2.2.5_amd64.deb && rm turbovnc_2.2.5_amd64.deb',
         'wget https://swift.rc.nectar.org.au/v1/AUTH_810/CVL-Singularity-External-Files/virtualgl_2.6.4_amd64.deb && dpkg -i virtualgl_2.6.4_amd64.deb && rm virtualgl_2.6.4_amd64.deb',
         'apt update',
         'apt -y upgrade'
     ])
-    stage0 += gnu(version='10')
+    stage0 += gnu(version=gnu_version, extra_repository=True)
     main_version = blender_version.rsplit('.', 1)[0]
     blender_source = f"https://ftp.nluug.nl/pub/graphics/blender/release/Blender{main_version}/blender-{blender_version}-linux64.tar.xz"
     prefix = '/usr/local/blender'
@@ -61,8 +61,8 @@ def build(container_format='singularity', os_release='ubuntu', os_version='18.04
 
     blenderGIS_source = "https://github.com/domlysz/BlenderGIS/archive/refs/tags/v2.2.4-10/2020.zip"
     stage0 += shell(commands=[
-        f"wget {blenderGIS_source} /var/tmp/",
-        f"{prefix}/blender -y -b --python-expr 'import bpy;bpy.ops.wm.addon_install(filepath=/var/tmp/2020.zip)'"
+        f"cd /var/tmp && wget {blenderGIS_source}",
+        f"{prefix}/blender -y -b --python-expr \'import bpy;bpy.ops.preferences.addon_install(filepath=\"/var/tmp/2020.zip\")\'"
     ])
 
 
